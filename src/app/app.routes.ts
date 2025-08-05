8b96b4e3dff460ef6e819867ba279e4329ab95de
die.eramos <die.eramos@gmail.com>
2025-08-04 22:50:37 -0300

    C:/Program Files/Git/dist/

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from "./pages/home/home";
+import { Asistencia } from "./pages/asistencia/asistencia";
+import { Ubicacion } from "./pages/ubicacion/ubicacion";
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    {path: '', component: Home},
+    {path: 'asistencia', component: Asistencia},
+    {path: 'ubicacion', component: Ubicacion},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Home } from "./pages/home/home";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Header } from './core/header/header';
+import { Asistencia } from './pages/asistencia/asistencia';
+import { Ubicacion } from './pages/ubicacion/ubicacion';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home , RouterLink, RouterLinkActive, Header,Asistencia,Ubicacion],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
